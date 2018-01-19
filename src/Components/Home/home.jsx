b0ee85228f0ffc824ee9ba16574644058f8b8e18
Brian Larson <manateeman24@hotmail.com>
2018-01-19 16:58:23 -0700

    actual last friday commit

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './../Header/header'; 
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 class Home extends Component {
     constructor(props){
         super(props)
         this.state = {
-            something: 'true'
+            something: 'true',
+            response: []
         }
         this.handleClick = this.handleClick.bind(this)
     }
@@ -20,13 +22,20 @@ class Home extends Component {
             something: 'neither true or false'
         })
     }
-
+componentDidMount(){
+    axios.get('https://swapi.co/api/people/1/').then((res) => {
+        this.setState({
+            response: res.data
+        })
+    })
+}
     render() {
         return (
             <div>
                 <Header data ="America"/>
                 <button onClick={this.handleClick}>Change State</button>       
             <h1>{this.state.something}</h1>  
+            <h2>{this.state.response.name}</h2>
             <input type="text" onChange={this.handleClick}/>
             <input type="text" onChange={this.handleChange} />
             <Link to = '/nothome'><button>Go home</button></Link>
